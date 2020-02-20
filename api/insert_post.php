13e856650de3c2fd1d3adb5ee875620641865a90
Edoardo Mileto <edomileto@gmail.com>
2020-02-20 20:10:59 +0100

    update api post

--- api/insert_post.php
+++ api/insert_post.php
@@ -2,12 +2,26 @@
 session_start();
 require_once("../dataaccess/post.php");
 
+if (!array_key_exists("correct", $_POST) || !array_key_exists("wrong", $_POST)) {
+        echo '{"code":0,"description":"missing parameter"}';
+        exit;
+    }else{
+
 $testo = "Ciao!\nHo giocato a Trivia ed ho fatto " . $_POST['correct'] . " risposte corrette e " . $_POST['wrong'] . " risposte sbagliate\nGioca anche te a trivia! <a href=\"../trivia\">Clicca qui</a>";
 
-echo '{"code":0,"description":"domande corrette: ' . $_POST['correct'] .'"}';
 $p = new Post();
 $p->message = $testo;
 $p->date = new DateTime();
 $p->user = $_SESSION["user_id"];
 $res = $p->save();
+
+if ($res)
+  {
+    echo '{"code":1,"description":"Success! Results posted"}';
+  }
+  else
+  {
+    echo '{"code":0,"description":"Something went wrong trying to insert your post"}';
+  }
+}
 ?>
